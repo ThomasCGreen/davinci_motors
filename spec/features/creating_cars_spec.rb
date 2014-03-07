b1393f4c8f9814ed22425492fc1386af6e977422
Thomas C Green <TomG@AccelHomeBuyers.com>
2014-03-07 17:58:55 +0000

    * added test for second car being added
    [#67063414]

--- spec/features/creating_cars_spec.rb
+++ spec/features/creating_cars_spec.rb
@@ -21,4 +21,47 @@ feature "Creating Cars" do
 
     expect(page).to have_content('2300') # check that price showed up in list
   end
+
+  scenario "can create a second car" do
+    visit '/'
+
+    click_link 'New Car'
+
+    fill_in 'Make', with: 'Ford'
+    fill_in 'Model', with: 'Mustang'
+    fill_in 'Year', with: '1967'
+    fill_in 'Price', with: '2300'
+
+    click_button 'Create Car'
+
+    expect(page).to have_content('1967 Ford Mustang created')
+    expect(page).to have_content('Make')  # Look for headers
+    expect(page).to have_content('Model')
+    expect(page).to have_content('Year')
+    expect(page).to have_content('Price')
+
+    expect(page).to have_content('2300') # check that price showed up in listclick_link 'New Car'
+
+    click_link 'New Car'
+
+    fill_in 'Make', with: 'Chevrolet'
+    fill_in 'Model', with: 'Suburban'
+    fill_in 'Year', with: '2004'
+    fill_in 'Price', with: '8000'
+
+    click_button 'Create Car'
+
+    expect(page).to have_content('2004 Chevrolet Suburban created')
+    expect(page).to have_content('Make')  # Look for headers
+    expect(page).to have_content('Model')
+    expect(page).to have_content('Year')
+    expect(page).to have_content('Price')
+
+    expect(page).to have_content('1967')  # Look for headers
+    expect(page).to have_content('Ford')
+    expect(page).to have_content('Mustang')
+    expect(page).to have_content('2300')
+
+    expect(page).to have_content('8000') # check that price showed up in list
+  end
 end
