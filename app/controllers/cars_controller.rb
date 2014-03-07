c5b3f709fc7999be170270441af21830e2a0720a
Thomas C Green <TomG@AccelHomeBuyers.com>
2014-03-07 19:18:12 +0000

    * added edit tests which required addition of *  edit, show and update methods
    [#67063414]

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,4 +1,5 @@
 class CarsController < ApplicationController
+  before_action :set_car, only: [:edit, :show, :update]
 
   def index
     @cars = Car.all
@@ -6,6 +7,9 @@ class CarsController < ApplicationController
 
   def new
     @car = Car.new
+  end
+
+  def show
   end
 
   def create
@@ -17,7 +21,28 @@ class CarsController < ApplicationController
     end
   end
 
+  def update
+    respond_to do |format|
+      if @car.update(car_params)
+        format.html { redirect_to @car, notice: 'Car was successfully updated.' }
+      else
+        format.html { render action: 'edit' }
+      end
+    end
+  end
+
+  def edit
+
+  end
+
   private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_car
+    @car = Car.find(params[:id])
+  end
+
+
   def car_params
     params.require(:car).permit([:year, :make, :model, :price])
   end
